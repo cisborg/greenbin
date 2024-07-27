59b1dc8ab9debbf3d85312fbc2c7c02941c676d7
craftmenke <craftmenke@gmail.com>
2024-07-27 18:17:02 +0000

    tabs working okay

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,34 +1,62 @@
 // app/_layout.js
 import { Tabs } from 'expo-router';
-import Icon from '../assets/icon';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+// Define tab configurations
+const tabConfig = [
+  {
+    name: 'homepage',
+    focusedIcon: 'home',
+    unfocusedIcon: 'home-outline',
+    iconComponent: Ionicons
+  },
+  {
+    name: 'challenges',
+    focusedIcon: 'lightning-bolt',
+    unfocusedIcon: 'lightning-bolt-outline',
+    iconComponent: MaterialCommunityIcons
+  },
+  {
+    name: 'news',
+    focusedIcon: 'menu',
+    unfocusedIcon: 'menu',
+    iconComponent: MaterialCommunityIcons
+  },
+  {
+    name: '[id]',
+    focusedIcon: 'user-circle-o',
+    unfocusedIcon: 'user-o',
+    iconComponent: FontAwesome
+  },
+];
+
+// Function to determine tab options based on route
+const tabOptions = ({ route }) => {
+  const routeConfig = tabConfig.find(config => config.name === route.name);
+  const iconName = routeConfig ? (routeConfig.focusedIcon) : '';
+  const IconComponent = routeConfig ? routeConfig.iconComponent : Ionicons; // Fallback to Ionicons if no config
+
+  return {
+    tabBarIcon: ({ focused, color, size }) => {
+      const iconName = focused ? routeConfig.focusedIcon : routeConfig.unfocusedIcon;
+      return <IconComponent name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: "white",
+    tabBarInactiveTintColor: 'white',
+    tabBarStyle: { backgroundColor: '#518265' },
+    tabBarLabel: '',
+  };
+};
 
 const TabLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="homepage" options={{
-         title: 'Home',
-         tabBarIcon: ({ color, size }) => (
-          <Home name="home" color={color} size={size} />
-        ),
-          }}  />
-      <Tabs.Screen name="challenges" options={{
-         title: 'Challenges',
-         tabBarIcon: ({ color, size }) => (
-          <Icon name="ios-flash" color={color} size={size} />
-        )
-          }}  />
-      <Tabs.Screen name="news" options={{
-         title: 'News',
-         tabBarIcon: ({ color, size }) => ( 
-          <Icon name="ios-menu" color={color} size={size} />
-        )
-          }}  />
-      <Tabs.Screen name="[id]" options={{
-         title: 'User',
-         tabBarIcon: ({ color, size }) => (
-          <Icon name="ios-person" color={color} size={size} />
-        )
-          }}  />
+    <Tabs screenOptions={tabOptions}>
+      <Tabs.Screen name="homepage" options={{}} />
+      <Tabs.Screen name="challenges" options={{}} />
+      <Tabs.Screen name="news" options={{}} />
+      <Tabs.Screen name="[id]" options={{}} />
     </Tabs>
   );
 };
--- app/assets/icons/icons_management.js
+++ app/assets/icons/icons_management.js
@@ -1,9 +1,7 @@
-// src/components/Icon.js
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { FileMusic, Home, Lightning, ListMenu, User } from './icons';
 
-// Create a mapping object for your SVGs
 const svgMap = {
   FileMusic,
   Home,
@@ -12,18 +10,14 @@ const svgMap = {
   User,
 };
 
-// Icon component to display SVGs based on the name prop
 const Icon = ({ name, width = 20, height = 20, color = "#000" }) => {
-  // Get the SVG component from the map
   const SvgComponent = svgMap[name];
 
-  // Check if the SVG component exists
   if (!SvgComponent) {
     console.error(`SVG with name "${name}" not found.`);
-    return null; // Or render a fallback UI
+    return null;
   }
 
-  // Render the SVG component with the specified props
   return (
     <View style={styles.container}>
       <SvgComponent width={width} height={height} fill={color} />
